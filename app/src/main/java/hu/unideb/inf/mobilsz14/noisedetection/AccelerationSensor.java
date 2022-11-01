5734636e997bfb1dabafc15f449697f3274b1638
szabesz <jakab.szabolcs226@gmail.com>
2022-11-01 15:38:42 +0100

    accelerationValues
    can distinguish the values from one and another

--- app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/AccelerationSensor.java
+++ app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/AccelerationSensor.java
@@ -1,13 +1,27 @@
 package hu.unideb.inf.mobilsz14.noisedetection;
 
+import android.graphics.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Vibrator;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 public class AccelerationSensor implements SensorEventListener
 {
+
+
+
+    private ProgressBar pb;
+    private TextView tv;
+    private Vibrator v;
+    private CameraManager t;
 
     public void setPb(ProgressBar pb)
     {
@@ -19,19 +33,88 @@ public class AccelerationSensor implements SensorEventListener
         this.tv = tv;
     }
 
-    private ProgressBar pb;
-    private TextView tv;
+    public void SetVibrate(Vibrator v)
+    {
+        this.v = v;
+    }
 
+    public void SetTorch(CameraManager t)
+    {
+        this.t = t;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onSensorChanged(SensorEvent sensorEvent)
     {
         pb.setProgress(Math.abs((int)sensorEvent.values[0]*5));
-//        tv.setText("" + sensorEvent.values[0]);
-        System.out.println(sensorEvent.values[0]);
+        //tv.setText("" + sensorEvent.values[0]);
+        //System.out.println(sensorEvent.values[0]);
+        //elágazások:
+        float AccelerationValues = Math.abs(sensorEvent.values[0]);
+        if(AccelerationValues == 0)
+        {
+            v.cancel();
+            tv.setText("It's steady.");
+        }
+        else if(AccelerationValues <= 2 && AccelerationValues >= 0.1)
+        {
+            v.cancel();
+            tv.setText("Well, it moves... Slowly");
+        }
+        else if(AccelerationValues <= 6 && AccelerationValues > 2)
+        {
+            v.cancel();
+            tv.setText("It's a great acceleration");
+            LightTorch(3);
+            //villog pl háromszor 2sec alatt
+        }
+        else if(AccelerationValues <= 12 && AccelerationValues > 6)
+        {
+            LightTorch(0);
+            v.cancel();
+            tv.setText("Woah, it's fast..");
+            v.vibrate(100);
+            LightTorch(8);
+            //gyorsabban villog és rezeg
+        }
+        else if (AccelerationValues > 12)
+        {
+            v.cancel();
+            tv.setText("Almost the speed of the light... :O");
+            //folyamatosan világít és statikusan rezeg
+            v.vibrate(10000);
+        }
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private String getCameraID;
+
+    public void LightTorch(int times) //torch flashes up count/second times
+    {
+        try {
+            getCameraID = t.getCameraIdList()[0];
+            t.setTorchMode(getCameraID, false);
+
+            for(int i = 0; i < times; i++)
+            {
+                //flash & sleep
+                t.setTorchMode(getCameraID, true);
+                Thread.sleep(10);
+                t.setTorchMode(getCameraID, false);
+                Thread.sleep((int)1000/times);
+
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/MainActivity.java
@@ -2,11 +2,14 @@ package hu.unideb.inf.mobilsz14.noisedetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
 import android.hardware.lights.Light;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.ProgressBar;
 import android.widget.Switch;
@@ -15,10 +18,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Vibrator vibrator;
     private Switch runOnBackGroundSwitch;
     private ProgressBar accelerationPb;
     private SensorManager sensorManager;
-//    private TextView sensorTextView, accelerationTv;
+    private CameraManager cameraManager;
+    private TextView sensorTextView, accelerationTv;
     private Sensor mLight;
     private AccelerationSensor sel = new AccelerationSensor();
 
@@ -30,6 +35,8 @@ public class MainActivity extends AppCompatActivity
 
         runOnBackGroundSwitch = findViewById(R.id.runOnBackgroundSwitch);
         accelerationPb = findViewById(R.id.accelerationPb);
+        accelerationTv = findViewById(R.id.accelerationTv);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 //        accelerationTv = findViewById(R.id.accelerationTv);
 
 
@@ -39,13 +46,15 @@ public class MainActivity extends AppCompatActivity
         mLight = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         sensorManager.registerListener(sel, mLight, SensorManager.SENSOR_DELAY_FASTEST);
-
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
         sel.setPb(accelerationPb);
-        System.out.println(accelerationPb.getProgress());
-        //sel.setTv(accelerationTv);
+        sel.setTv(accelerationTv);
+        sel.SetVibrate(vibrator);
+        sel.SetTorch(cameraManager);
         //sensorTextView.setMovementMethod((new ScrollingMovementMethod()));
         //sensorTextView.setText(sensors.toString());
 
     }
+
 }
