80b97820a15a4bf63e5147515230c17eb51deacd
szabesz <jakab.szabolcs226@gmail.com>
2022-11-14 15:29:26 +0100

    final first version
    minor changes in the program's basics

--- app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/AccelerationSensor.java
+++ app/src/main/java/hu/unideb/inf/mobilsz14/noisedetection/AccelerationSensor.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Vibrator;
 import android.widget.ProgressBar;
@@ -18,8 +19,7 @@ public class AccelerationSensor implements SensorEventListener {
 
     private ProgressBar pb;
     private TextView tv;
-    private Vibrator v;
-    private CameraManager t;
+    private MediaPlayer ws;
 
     public void setPb(ProgressBar pb) {
         this.pb = pb;
@@ -29,63 +29,33 @@ public class AccelerationSensor implements SensorEventListener {
         this.tv = tv;
     }
 
-    public void SetVibrate(Vibrator v) {
-        this.v = v;
-    }
+    public void setMedia(MediaPlayer winSound){ this.ws = winSound;}
 
-    public void SetTorch(CameraManager t) {
-        this.t = t;
-    }
+    public float maxAcceleration = 0;
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         try {
-
-            getCameraID = t.getCameraIdList()[0];
-            pb.setProgress(Math.abs((int) sensorEvent.values[0] * 5));
             //tv.setText("" + sensorEvent.values[0]);
             //System.out.println(sensorEvent.values[0]);
             //elágazások:
             float AccelerationValues = Math.abs(sensorEvent.values[0]);
-            if (AccelerationValues <= 2) {
-                t.setTorchMode(getCameraID, false);
-                v.cancel();
-                tv.setText("It's steady.");
-            }
-            else if (AccelerationValues <= 4 && AccelerationValues > 2)
+            pb.setProgress((int)Math.abs(AccelerationValues / 78 * 100));
+            if(maxAcceleration<AccelerationValues)
             {
-                t.setTorchMode(getCameraID, false);
-                v.cancel();
-                tv.setText("Well, it moves... Slowly");
+                maxAcceleration = AccelerationValues;
             }
-            else if (AccelerationValues <= 8 && AccelerationValues > 4)
+            tv.setText((int)(maxAcceleration/78*100)+"%");
+            if(AccelerationValues > 78)
             {
-                t.setTorchMode(getCameraID, false);
-                Thread.sleep(10);
-                v.cancel();
-                tv.setText("It's a great acceleration");
-                t.setTorchMode(getCameraID, true);
-
-                //villog pl háromszor 2sec alatt
-            } else if (AccelerationValues <= 16 && AccelerationValues > 8) {
-                t.setTorchMode(getCameraID, false);
-                Thread.sleep(10);
-                v.cancel();
-                tv.setText("Woah, it's fast..");
-                v.vibrate(100);
-                t.setTorchMode(getCameraID, true);
-                //gyorsabban villog és rezeg
-            } else if (AccelerationValues > 16) {
-                t.setTorchMode(getCameraID, false);
-                Thread.sleep(10);
-                v.cancel();
-                tv.setText("Almost the speed of the light... :O");
-                //folyamatosan világít és statikusan rezeg
-                v.vibrate(200);
-                t.setTorchMode(getCameraID, true);
+                ws.start();
+                tv.setText("WOAH, youre strong.");
+                Thread.sleep(2000);
             }
-        } catch (CameraAccessException | InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
@@ -97,6 +67,5 @@ public class AccelerationSensor implements SensorEventListener {
 
     }
 
-    private String getCameraID;
 }
 
